ecfb1b09450d3179edcd14b9a15e80527fd481e2
Gabriel Costa Ganassin <gabriel.ganassin@contabilizei.com.br>
2023-11-16 15:53:35 -0300

    tests: finishing linked list unit tests

--- linked_list/linked_list_test.go
+++ linked_list/linked_list_test.go
@@ -7,36 +7,43 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var second = linkedlist.ListNode{
-	Value: 2,
-}
+func getHead() linkedlist.ListNode {
+	var second = linkedlist.ListNode{
+		Value: 2,
+	}
 
-var first = linkedlist.ListNode{
-	Value: 1,
-	Next:  &second,
-}
+	var first = linkedlist.ListNode{
+		Value: 1,
+		Next:  &second,
+	}
 
-var head = linkedlist.ListNode{
-	Value: 0,
-	Next:  &first,
+	return linkedlist.ListNode{
+		Value: 0,
+		Next:  &first,
+	}
 }
 
 func TestGetLastNodet(t *testing.T) {
+
+	head := getHead()
+
 	last := linkedlist.GetLastNode(&head)
 
-	assert.Equal(t, last, &second)
+	assert.Equal(t, last.Value, 2)
 }
 
 func TestFind(t *testing.T) {
 
 	t.Run("should find existing value", func(t *testing.T) {
+		head := getHead()
 
 		found := linkedlist.Find(&head, 1)
 
-		assert.Equal(t, &first.Value, found)
+		assert.Equal(t, 1, *found)
 	})
 
 	t.Run("should return nil if value not in the list", func(t *testing.T) {
+		head := getHead()
 
 		found := linkedlist.Find(&head, 9)
 
@@ -44,9 +51,39 @@ func TestFind(t *testing.T) {
 	})
 
 	t.Run("should return nil if head nil", func(t *testing.T) {
-
 		found := linkedlist.Find(nil, 9)
 
 		assert.Nil(t, found)
+	})
+}
+
+func TestPushFront(t *testing.T) {
+	t.Run("should add node to the beginning", func(t *testing.T) {
+		head := getHead()
+
+		node := linkedlist.ListNode{
+			Value: 9,
+		}
+
+		newHead := linkedlist.PushFront(&head, &node)
+
+		assert.Equal(t, &node, newHead)
+		assert.Equal(t, &head, newHead.Next)
+	})
+}
+
+func TestPushEnd(t *testing.T) {
+	t.Run("should add node to the end", func(t *testing.T) {
+		head := getHead()
+
+		node := linkedlist.ListNode{
+			Value: 9,
+		}
+
+		linkedlist.PushEnd(&head, &node)
+
+		last := linkedlist.GetLastNode(&head)
+
+		assert.Equal(t, &node, last)
 	})
 }
