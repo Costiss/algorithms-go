2d234882720e0a6537ba625fc3fc40c57079962b
Gabriel Costa Ganassin <gabriel.ganassin@contabilizei.com.br>
2023-11-20 08:58:19 -0300

    feat: Binary Trees - In Order Traversal

--- trees/bt_pre_order.go
+++ trees/bt_pre_order.go
@@ -1,24 +1,18 @@
 package trees
 
-type BinaryNode struct {
-	Value int
-	Left  *BinaryNode
-	Right *BinaryNode
-}
-
 func PreOrderTraversal(root BinaryNode) []int {
-	return walk(&root, []int{})
+	return walkPreOrder(&root, []int{})
 }
 
-func walk(node *BinaryNode, path []int) []int {
+func walkPreOrder(node *BinaryNode, path []int) []int {
 	if node == nil {
 		return path
 	}
 
 	path = append(path, node.Value)
 
-	path = walk(node.Left, path)
-	path = walk(node.Right, path)
+	path = walkPreOrder(node.Left, path)
+	path = walkPreOrder(node.Right, path)
 
 	return path
 }
